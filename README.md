02a8844f2179035ee2b276be77adb125df67c246
D. Palacio <dapasa@har.upv.es>
2023-05-14 22:21:50 +0200

    Mensaje de bienvenida

--- README.md
+++ README.md
@@ -2,10 +2,8 @@
 
 ## Control de acceso
 
-### Implementar el endpoint GET
+### Saludo de bienvenida
 
-En el archivo `api/sesion/index.php`, por ejemplo con una sentencia `switch`,
-definimos qué se ejecutará si el método de la petición HTTP es POST (lo que ya
-teníamos hecho) o GET. En este último caso, comprobamos si existe una sesión
-válida y si es así, devolvemos los datos del usuario, y si no devolvemos un
-error 401 
+Para acabar de refinar el resultado podemos hacer que el contenido de la página
+no se muestre mientras se obtiene la respuesta del servidor, y que si es
+correcta, se muestre el mensaje de bienvenida
--- src/app/control-acceso.js
+++ src/app/control-acceso.js
@@ -1,13 +1,12 @@
-/* Como queremos que se ejecute inmediatamente ponemos el código en una
-   función auto-ejecutable */
 (async () => {
-    // petición a la API, si no se especifica, será por el método GET
     const respuesta = await fetch('../api/sesion/');
-    // Si la respuesta es correcta...
     if(respuesta.ok) {
         // procesar la respuesta
         const data = await respuesta.json();
-        console.log(data);
+        // mostrar el nombre del usuario en el mensaje de bienvenida
+        document.getElementById('user-name').innerText = data.nombre;
+        // mostrar el contenido de la página
+        document.body.classList.remove("loading");
     } else {
         location.href = '../';
     }
