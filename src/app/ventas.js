df255a31355fbbdbdfdfb1b6e219d840dafa890b
Daniel Palacio <dapasa@har.upv.es>
2023-05-30 10:02:53 +0200

    Petición num. páginas

--- src/app/ventas.js
+++ src/app/ventas.js
@@ -1,12 +1,25 @@
 class VentasModel {
     url = '../api/ventas/';
 
-    async getVentas(cantidad) {
-        const respuesta = await fetch(this.url+'?cantidad='+cantidad);
+    constructor(ventasPorPagina) {
+        this.ventasPorPagina = ventasPorPagina;
+    }
+
+    async getVentas() {
+        const respuesta = await fetch(this.url+'?cantidad='+this.ventasPorPagina);
         if(!respuesta.ok) {
             return false;
         }
         this.ventas = await respuesta.json();
         return this.ventas;
+    }
+
+    async getPaginas() {
+        const respuesta = await fetch(this.url+'/paginas/?cantidad='+this.ventasPorPagina);
+        if(!respuesta.ok) {
+            return false;
+        }
+        this.paginas = await respuesta.json();
+        return this.paginas;
     }
 }
